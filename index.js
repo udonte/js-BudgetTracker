f46f7022d2bd209980e9c85dfcceed325f3bf114
George Udonte <georgeudonte@gmail.com>
2023-12-29 15:11:00 +0100

    added add transaction list to income or expense

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const form = document.querySelector(".add");
+const incomeList = document.querySelector("ul.income-list");
+const expenseList = document.querySelector("ul.expense-list");
 let transactions =
   localStorage.getItem("transactions") != null
     ? JSON.parse(localStorage.getItem("transactions"))
     : [];
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+
+// generate list template function
+function generateTemplate(id, source, amount, time) {
+  return `<li data-id="${id}">
+      <p>
+        <span>${source}</span>
+        <span id="time">${time}</span>
+      </p>
+      $<span>${Math.abs(amount)}</span>
+      <i class="bi bi-trash delete"></i>
+    </li>`;
+}
+
+// add transaction to DOM
+function addTransactionDOM(id, source, amount, time) {
+  if (amount >= 0) {
+    incomeList.innerHTML += generateTemplate(id, source, amount, time);
+  } else {
+    expenseList.innerHTML += generateTemplate(id, source, amount, time);
+  }
+}
+
+//add new transaction
+function addTransaction(source, amount) {
   const time = new Date();
   const transaction = {
     id: Math.floor(Math.random() * 100000),
-    source: form.source.value.trim(),
-    amount: form.amount.value.trim(),
-    time: `${time.toLocaleTimeString()} ${time.toLocaleDateString()}}`,
+    source: source,
+    amount: amount,
+    time: `${time.toLocaleTimeString()} ${time.toLocaleDateString()}`,
   };
   transactions.push(transaction);
   localStorage.setItem("transactions", JSON.stringify(transactions));
+  addTransactionDOM(transaction.id, source, amount, transaction.time);
+}
+
+// form details
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTransaction(form.source.value, form.amount.value);
+  form.reset();
 });
